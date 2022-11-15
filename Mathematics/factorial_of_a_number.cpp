26591391e74b0d236accaf6bd33b85fde75236b4
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-15 23:29:50 +0530

    Factorial of a number

---
+++ Mathematics/factorial_of_a_number.cpp
@@ -0,0 +1,35 @@
+// Factorial of a number
+
+#include<iostream>
+using namespace std;
+
+// Iterative Solution
+int fact(int n){
+    int res = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        res = res * i;
+    }
+    return res;
+}
+// Time complexity -> O(n)
+// Auxiliary Space: O(1)
+
+
+// Recursive solution
+int rec_fact(int n){
+    // Base conditon
+    if (n == 0)
+        return 1;
+    return n * rec_fact(n-1);
+}
+// Time complexity -> O(n)
+// Auxiliary Space: O(n)
+
+int main()
+{
+    int num = 5;
+    cout << "Factorial of " << num << " is " << fact(num) << endl;
+    cout << "Factorial of " << num << " is " << rec_fact(num) << endl;
+    return 0;
+}
