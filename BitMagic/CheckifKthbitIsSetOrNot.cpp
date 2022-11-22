d66a9b2d6f61fb12b71af39bc6dceffc2c30bc79
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-22 23:57:33 +0530

    Check if Kth bit is set or not.

---
+++ BitMagic/CheckifKthbitIsSetOrNot.cpp
@@ -0,0 +1,58 @@
+// Check if Kth bit is set or not.
+
+#include<iostream>
+using namespace std;
+
+// Naive approch
+void isKthBitSet(int n, int k){
+    int x = 1;
+    for(int i=0; i<(k-1); i++)
+        x = x*2;
+    if((n&x) != 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+// Alternate naive solution
+void isKthBitSet_secondApproch(int n, int k){
+    int x = 1;
+    for(int i=0; i<(k-1); i++)
+        n = n / 2;
+    if((n&x) != 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+// Efficient maehod 1
+void isKthBitSet_effectiveApproch1(int n, int k){
+    int x = (1 << (k-1));
+    if((n&x) != 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+// Efficient maehod 2
+void isKthBitSet_effectiveApproch2(int n, int k){
+    int x = (n >> (k-1));
+    if((n&x) != 0)
+        cout << "Yes";
+    else
+        cout << "No";
+}
+
+int main()
+{
+    int n = 5, k = 3;
+    // Function call
+    isKthBitSet(n, k);
+    cout << endl;
+    isKthBitSet_secondApproch(n, k);
+    cout << endl;
+    isKthBitSet_effectiveApproch1(n, k);
+    cout << endl;
+    isKthBitSet_effectiveApproch2(n, k);
+    return 0;
+}
