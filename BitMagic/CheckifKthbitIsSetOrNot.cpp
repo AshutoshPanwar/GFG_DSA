12fdf90a579459f727f5716df514847b74a1e94d
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-23 00:11:17 +0530

    Check if Kth bit is set or not.

--- BitMagic/CheckifKthbitIsSetOrNot.cpp
+++ BitMagic/CheckifKthbitIsSetOrNot.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void isKthBitSet(int n, int k){
     int x = 1;
     for(int i=0; i<(k-1); i++)
-        x = x*2;
+        x = x*2;        // 2^(k-1)
     if((n&x) != 0)
         cout << "Yes";
     else
