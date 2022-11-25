86b4b229bfaefed8eb66b8afcc6db5819271131b
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-25 23:31:29 +0530

    Solution

---
+++ BitMagic/Problems/RightmostDifferentBit.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        if(m == n)
+            return -1;
+        int x = m^n;
+        int count = 0;
+        while(x > 0){
+            count++;
+            if((x%2) == 1){
+                return count;
+            }
+            x=x>>1;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Endss
