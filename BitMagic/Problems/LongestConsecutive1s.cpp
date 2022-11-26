916dc691ad2885cd5322a9495442d34f75eee653
AshutoshPanwar <ashupanwar1100@gmail.com>
2022-11-26 20:01:37 +0530

    Solution

---
+++ BitMagic/Problems/LongestConsecutive1s.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*  Function to calculate the longest consecutive ones
+*   N: given input to calculate the longest consecutive ones
+*/
+class Solution
+{
+    public:
+    int maxConsecutiveOnes(int N)
+    {
+        // code here
+        int current = 0;
+        int next = 0;
+        while(N > 0){
+            if((N&1) == 0){
+                current=0;
+            }
+            else{
+                current++;
+            }
+            N=N>>1;
+            if(current > next){
+                next = current;
+            }
+        }
+        return next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		Solution obj;
+		//calling maxConsecutiveOnes() function
+		cout<<obj.maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
