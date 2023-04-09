d6c12b76ebbeb2c87fce465e7994de8f7e44c6ab
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-10 00:22:43 +0530

    Working of Function calling through Recursion

--- Recursion/guess_the_output1.cpp
+++ Recursion/guess_the_output1.cpp
@@ -1,20 +1,30 @@
 // Gess the output to see how recursion works.
-// for better undestanding use the funciton call-stack.
+// for better undestanding use the funciton call-stack diagram.
 
 #include<iostream>
 using namespace std;
 
 void fun(int n){
-    if(n==0){
+    if(n==0){       // Base or Termination condition to stop recursion.
         return;
     }
     cout << n << endl;
-    fun(n-1);
+    fun(n-1);       // Recursive calling of function to self.
     cout << n << endl;
 }
 
 int main()
 {
-    fun(3);
+    fun(3);     // Parameterized function calling
     return 0;
 }
+
+
+
+// OUTPUT:
+//     3
+//     2
+//     1
+//     1
+//     2
+//     3
