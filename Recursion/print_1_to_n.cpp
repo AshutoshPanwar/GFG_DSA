6493eb2c165e0de4ad06828461450043f82a65ab
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-10 00:56:02 +0530

    Print No in Ascending order

--- Recursion/print_1_to_n.cpp
+++ Recursion/print_1_to_n.cpp
@@ -1,12 +1,14 @@
-// Print number from 1 to n.
+// Print number from 1 to n using recursion.
+
+// Return -> Counting in ascending order
 
 #include<iostream>
 using namespace std;
 
 void fun(int n){
-    if(n==0)
+    if(n==0)            // Base condition.
         return;
-    fun(n-1);
+    fun(n-1);           // Recursive call to function to itself.
     cout << n << endl;
 }
 
@@ -15,3 +17,10 @@ int main()
     fun(5);
     return 0;
 }
+
+// OUTPUT:
+    // 1
+    // 2
+    // 3
+    // 4
+    // 5
