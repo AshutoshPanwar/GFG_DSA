059905d9f21fd1ac098d1d6074ddcdef8b064df5
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-10 00:32:13 +0530

    Test your understanding of recursion

--- Recursion/guess_the_output2.cpp
+++ Recursion/guess_the_output2.cpp
@@ -5,16 +5,25 @@
 using namespace std;
 
 void fun(int n){
-    if(n==0){
+    if(n==0){       // Base condition.
         return;
     }
-    fun(n-1);
+    fun(n-1);       // Recursive Function calling.
     cout << n << endl;
-    fun(n-1);
+    fun(n-1);       // Recursive function calling
 }
 
 int main()
 {
-    fun(3);
+    fun(3);         // Parameterize function call
     return 0;
 }
+
+// OUTPUT:
+// 1
+// 2
+// 1
+// 3
+// 1
+// 2
+// 1
