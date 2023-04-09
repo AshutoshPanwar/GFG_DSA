edba383ffe278e9531b4231f62cf4f72b51b80ff
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-10 00:41:27 +0530

    Calculate Log(n)Base2 of any No

--- Recursion/guess_the_output3.cpp
+++ Recursion/guess_the_output3.cpp
@@ -2,6 +2,7 @@
 // for better undestanding use the funciton call-stack.
 
 // Return Log(n)base-2
+// We will get same ans for both even & odd values because we are explicitly converting to int.
 
 #include<iostream>
 using namespace std;
@@ -11,7 +12,7 @@ int fun(int n){
         return 0;
     }
     else
-        return 1+fun(n/2);
+        return 1+fun(n/2);      // Change 2 to any int then we will get log base of that no.
 }
 
 int main()
@@ -19,3 +20,6 @@ int main()
     cout << fun(16);
     return 0;
 }
+
+// OUTPUT:
+//     4
