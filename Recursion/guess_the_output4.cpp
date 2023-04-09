f22113fddfd09a423906666a6a0cf8a98e47ab1b
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-10 00:45:54 +0530

    Covert any No to it's binary equivalent

--- Recursion/guess_the_output4.cpp
+++ Recursion/guess_the_output4.cpp
@@ -1,17 +1,17 @@
 // Gess the output to see how recursion works.
 // for better undestanding use the funciton call-stack.
 
-// Decimal to Binary
+// Return -> Binary equivalent to that no.
 
 #include<iostream>
 using namespace std;
 
 void fun(int n){
-    if(n==0){
+    if(n==0){       // Base Case
         return;
     }
-    fun(n/2);
-    cout << (n%2);
+    fun(n/2);       // Recursive funtion calling
+    cout << (n%2);  // Print Remainder in reverse order to construct Binary value.
 }
 
 int main()
@@ -19,3 +19,7 @@ int main()
     fun(7);
     return 0;
 }
+
+
+// OUTPUT:
+//      111
