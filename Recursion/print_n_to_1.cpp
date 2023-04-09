d159aff8fa086b071ff8c80f3d801c66af417a64
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-10 00:58:09 +0530

    Print No in Descending order

--- Recursion/print_n_to_1.cpp
+++ Recursion/print_n_to_1.cpp
@@ -1,12 +1,14 @@
 // Print number from n to 1.
 
+// Return -> Counting in DESCENDING order
+
 #include<iostream>
 using namespace std;
 
 void fun(int n){
-    if(n==0)
+    if(n==0)            // Base condition.
         return;
-    cout << n << endl;
+    cout << n << endl;  // Recursive call to function to itself.
     fun(n-1);
 }
 
@@ -15,3 +17,10 @@ int main()
     fun(5);
     return 0;
 }
+
+// OUTPUT:
+//     5
+//     4
+//     3
+//     2
+//     1
