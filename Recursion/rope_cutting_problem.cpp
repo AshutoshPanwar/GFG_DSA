a5d6a232860d5f6ea76d5166f47e46c2b1986c75
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-11 00:56:12 +0530

    Rope cutting Problem

---
+++ Recursion/rope_cutting_problem.cpp
@@ -0,0 +1,36 @@
+// Rope Cutting Probelm -> Maximum No of Pieces in which a rope can cut depending upon provided value's.
+// Use Recursion Tree method to better understand problem.
+// No of cut's [0 < a,b,c <= n]
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+
+int maxCuts(int n, int a, int b, int c)
+{
+    // Base Condition's
+    if(n == 0)
+        return 0;
+    if(n <= -1)
+        return -1;
+
+    int res = max(maxCuts(n-a, a, b, c),
+              max(maxCuts(n-b, a, b, c),
+              maxCuts(n-c, a, b, c)));
+
+    if(res == -1)
+        return -1;
+    return res + 1;
+}
+
+int main()
+{
+    int n = 5, a = 2, b = 1, c = 5;
+
+    cout << maxCuts(n, a, b, c);
+    return 0;
+}
+
+// OUTPUT:
+//     5
