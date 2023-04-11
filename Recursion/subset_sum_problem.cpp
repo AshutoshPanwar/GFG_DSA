b53192bef9d703830d60e6957f795b24022e7735
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-11 15:18:53 +0530

    Check Sum as subset Sum

--- Recursion/subset_sum_problem.cpp
+++ Recursion/subset_sum_problem.cpp
@@ -1,13 +1,16 @@
 // Subset Sum Problem (Any Subset of a given set contain sum as given sum value)
+// For a given set ther are 2^n subset's.
+// In this we are focusing on sum variable in recursion tree if(0) then possible else not.
 
 #include<iostream>
 using namespace std;
 
-int isSubsetSum(int arr[], int n, int sum){
-    if(n==0){
-        return (sum == 0) ? 1:0;
+int isSubsetSum(int ARR[], int N, int SUM){
+    if(N == 0){                                 // Base condition
+        return (SUM == 0) ? 1 : 0;
     }
-    return isSubsetSum(arr, n-1, sum) + isSubsetSum(arr, n-1, sum-arr[n-1]);
+
+    return isSubsetSum(ARR, N-1, SUM) + isSubsetSum(ARR, N-1, SUM-ARR[N-1]);        // Recursive call
 }
 
 int main()
@@ -15,9 +18,14 @@ int main()
     int set[] = {10, 20, 15 };
     int sum = 25;
     int n = sizeof(set) / sizeof(set[0]);
+
     if (isSubsetSum(set, n, sum) == true)
-        cout <<"Found a subset with given sum";
+        cout <<"Found a subset with given sum" << endl;
     else
-        cout <<"No subset with given sum";
+        cout <<"No subset with given sum" << endl;
+
     return 0;
 }
+
+// OUTPUT:
+//       Found a subset with given sum
