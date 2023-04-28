736693b250aaeedcaaa3e564385f716a273bbb81
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-04-28 23:57:54 +0530

    Sliding window technique

--- Array/Sliding_window_technique.cpp
+++ Array/Sliding_window_technique.cpp
@@ -1,7 +1,9 @@
+// Sliding Window Problem.
+
 #include<iostream>
 using namespace std;
 
-int maxSum(int arr[], int n, int k)
+int Naive_maxSum(int arr[], int n, int k)
 {
     int res = INT_MIN;
     for (int i = 0; i+k-1 < n; i++)
@@ -16,9 +18,31 @@ int maxSum(int arr[], int n, int k)
     return res;
 }
 
+int maxSum(int arr[], int n, int k)
+{
+    int res = 0;
+    for (int i = 0; i < k; i++)
+    {
+        res += arr[i];
+    }
+
+    int curr = res;
+
+    for (int i = k; i < n; i++)
+    {
+        res += (arr[i] - arr[i-k]);
+        curr = max(res, curr);
+    }
+    return curr;
+}
+
 int main()
 {
     int arr[] = {1, 8, 30, -5, 20, 7}, n = 6, k = 3;
     cout << maxSum(arr, n, k) << endl;
     return 0;
 }
+
+
+// OUTPUT:
+//     45
