8c3103bfd4d4b0a7fa8d16d0be76bfb3fbdf0ab1
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-05-03 00:17:20 +0530

    Reverse an Array

---
+++ Array/Reverse_an_Array.cpp
@@ -0,0 +1,44 @@
+// Reverse element's of an Array.
+
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n)
+{
+    int low = 0, high = n - 1;      // We maintain two pointer's and swap the element's
+
+    while (low < high)
+    {
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+        low++;
+        high--;
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 5, 7, 30}, n = 4;
+    cout << "Before Reverse" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    reverse(arr, n);
+    cout << "After Reverse" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+
+// OUTPUT:
+//     Before Reverse
+//     10 5 7 30
+//     After Reverse
+//     30 7 5 10
