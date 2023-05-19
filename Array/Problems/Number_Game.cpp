982a6bf1597013a8afa01f7ba3a9b6167d43e91f
AshutoshPanwar <ashupanwar1100@gmail.com>
2023-05-19 23:51:50 +0530

    LCM from 1 to n

---
+++ Array/Problems/Number_Game.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    long long gcd(long long a, long long b){
+        if(b == 0)
+            return a;
+        else
+            return gcd(b, a%b);
+    }
+    long long numGame(long long n) {
+        // code here
+        if(n==1)
+            return 1;
+        
+        long long ans = 1;
+        
+        for(long long i = 2; i <= n; i++){
+            long long hcf = gcd(i, ans);
+            ans = (ans*i/hcf)%1000000007;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.numGame(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
